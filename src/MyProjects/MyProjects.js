799ce6da3551999dfcd1e0caacf4e798d218a239
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2021-04-14 13:02:54 -0400

    Add styling

--- src/MyProjects/MyProjects.js
+++ src/MyProjects/MyProjects.js
@@ -1,4 +1,4 @@
-import './MyProjects.css'
+import './MyProjects.scss'
 import React from 'react';
 import { Component } from 'react';
 import myProjectsList from './MyProjectsList.js';
@@ -13,12 +13,12 @@ class MyProjects extends Component {
     render(){
         // console.log(this.state.myProjectsList)
         return(
-            <main className = 'project-main'>
+            <main className ='project-main'>
                 {this.state.myProjectsList.map((project, index) => {
                     return <article key={index} className='project-section'>
                     <h1 key={project.projectName} className='project-title'>{ project.projectName }</h1>
                     <section key={project.createdUsing}className='created-using'>{ project.createdUsing.map((skill, index) => {
-                            return <p className='skill-list' key={index}> {skill} </p>
+                            return <a className='skill-list' key={index}> {skill} </a>
                         }) }
                     </section>
                     <section className='project-description' key={project.projectDescription}>{project.projectDescription}</section>
