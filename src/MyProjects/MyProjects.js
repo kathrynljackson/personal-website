5e7808dd24cfd4ef6b2a53475e12505d7bb496b6
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2021-04-14 11:41:04 -0400

    Successfully render project data in myProjects

--- src/MyProjects/MyProjects.js
+++ src/MyProjects/MyProjects.js
@@ -1,20 +1,26 @@
 import './MyProjects.css'
 import React from 'react';
 import { Component } from 'react';
+import myProjectsList from './MyProjectsList.js';
+
 
 class MyProjects extends Component {
     constructor() {
         super()
-        this.state={
-
-        }
+        this.state = {myProjectsList};
     }
-
+ 
     render(){
+        console.log(this.state.myProjectsList)
         return(
-            <div>
-
-            </div>
+            <main className = 'project-main'>
+                {this.state.myProjectsList.map((project, index) => {
+                    return <section key={index} className='project-section'>
+                    <h1 key={project.projectName} className='project-title'>{ project.projectName }</h1>
+                    <p key={project.createdUsing}className='created-using'>{ project.createdUsing }</p>
+                    </section>
+                })}
+            </main>
         )
     }
 }
--- src/MyProjects/MyProjectsList.js
+++ src/MyProjects/MyProjectsList.js
@@ -1,21 +1,25 @@
-export default myProjectsList = [
+const myProjectsList = [
     {
         projectName: 'name',
+        projectDescription: 'description1',
         projectLink: 'link',
         projectImage: 'imagelink',
         createdUsing: ['React', 'JavaScript', 'CSS']
     },
     {
         projectName: 'name1',
+        projectDescription: 'description2',
         projectLink: 'link1',
         projectImage: 'imagelink1',
         createdUsing: ['React1', 'JavaScript1', 'CSS1']
     },
     {
         projectName: 'name2',
+        projectDescription: 'description3',
         projectLink: 'link2',
         projectImage: 'imagelink2',
         createdUsing: ['React2', 'JavaScript2', 'CSS2']
     } 
-
 ]
+
+export default myProjectsList
